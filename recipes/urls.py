4c825e65786a2d7277c97b6ffa1edfc3b5995977
Giovanelli <ramongiovanelli2@gmail.com>
2024-01-29 17:39:15 -0300

    Continuacao da aula2

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),  # 127.0.0.1:8000/home/
-    path('sobre/', sobre),
-    path('contato/', contato)
+
 ]
