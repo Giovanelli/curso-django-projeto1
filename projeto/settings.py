b782cf5496b75f7cde0c56c04ac06aa399e0b663
Giovanelli <ramongiovanelli2@gmail.com>
2024-02-02 05:13:22 -0300

    update

--- projeto/settings.py
+++ projeto/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# arquivos static fora do app
+STATICFILES_DIR = [
+    BASE_DIR / 'base_static',
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from recipes.views import home
 
+# Quando for importar um arquivo irmão da mesma pasta.
+# Significa: da pasta onde eu estou import views.
+from . import views
+
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),  # 127.0.0.1:8000/home/
-
+    path('', views.home, name="home"),  # 127.0.0.1:8000/home/
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
+
+# o parâmetro name no função path é para dar um nome único para URL
